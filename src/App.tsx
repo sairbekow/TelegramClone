d23f72828cb3f839aab4bbfe8e5219293a049ea7
Kalnur <kalnur.go@gmail.com>
2022-10-29 22:21:15 +0600

    contr

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
 import { useState } from 'react'
 import reactLogo from '@/assets/icons/react.svg'
+import CreatorButton from './components/creatorButton'
+import RightColumn from './components/rightColumn'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDrawerOpened, setIsDrawerOpened] = useState(false)
+
+  const toggleDrawer =
+    (open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+      setIsDrawerOpened(open);
+    };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CreatorButton/>
+      <RightColumn isOpen={isDrawerOpened} toggleDrawer={toggleDrawer}/>
     </div>
   )
 }
